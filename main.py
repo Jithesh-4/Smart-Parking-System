e2a32df9235d1aa8bf15bf13480a9ba98d64dd24
Jithesh S <125364788+Jithesh-4@users.noreply.github.com>
2025-01-19 16:18:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,6 @@ with open('polygons', 'rb') as f:
     posList = pickle.load(f)
 
 
-def empty(a):
-    pass
-
-
-cv2.namedWindow("Vals")
-cv2.resizeWindow("Vals", 640, 240)
-cv2.createTrackbar("Val1", "Vals", 25, 50, empty)
-cv2.createTrackbar("Val2", "Vals", 16, 50, empty)
-cv2.createTrackbar("Val3", "Vals", 5, 50, empty)
-
-
 def checkSpaces():
     spaces = 0
     for pos in posList:
@@ -29,40 +18,33 @@ def checkSpaces():
         imgCrop = imgThres[y:y + h, x:x + w]
         count = cv2.countNonZero(imgCrop)
 
-        if count < 900:
+        if count < 850:
             color = (0, 200, 0)
-            thic = 5
+            thic = 3
             spaces += 1
-
         else:
             color = (0, 0, 200)
             thic = 2
 
         cv2.rectangle(img, (x, y), (x + w, y + h), color, thic)
+        cv2.putText(img, str(count), (x, y + h - 6), cv2.FONT_HERSHEY_PLAIN, 1, color, 2)
 
-        cv2.putText(img, str(cv2.countNonZero(imgCrop)), (x, y + h - 6), cv2.FONT_HERSHEY_PLAIN, 1,
-                    color, 2)
-
-    cvzone.putTextRect(img, f'Free: {spaces}/{len(posList)}', (50, 60), thickness=3, offset=20,
-                       colorR=(0, 200, 0))
+    cvzone.putTextRect(img, f'Free: {spaces}/{len(posList)}', (50, 60), thickness=3, offset=20, colorR=(0, 200, 0))
 
 
 while True:
-
-    # Get image frame
     success, img = cap.read()
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
         cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-    # img = cv2.imread('img.png')
+
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
-    # ret, imgThres = cv2.threshold(imgBlur, 150, 255, cv2.THRESH_BINARY)
 
-    val1 = cv2.getTrackbarPos("Val1", "Vals")
-    val2 = cv2.getTrackbarPos("Val2", "Vals")
-    val3 = cv2.getTrackbarPos("Val3", "Vals")
+    # Set fixed thresholding values
+    val1, val2, val3 = 25, 16, 5
     if val1 % 2 == 0: val1 += 1
     if val3 % 2 == 0: val3 += 1
+
     imgThres = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                      cv2.THRESH_BINARY_INV, val1, val2)
     imgThres = cv2.medianBlur(imgThres, val3)
@@ -70,11 +52,8 @@ while True:
     imgThres = cv2.dilate(imgThres, kernel, iterations=1)
 
     checkSpaces()
-    # Display Output
 
-    cv2.imshow("Image", img)
-    # cv2.imshow("ImageGray", imgThres)
-    # cv2.imshow("ImageBlur", imgBlur)
+    cv2.imshow("Video", img)
     key = cv2.waitKey(1)
     if key == ord('r'):
         pass
